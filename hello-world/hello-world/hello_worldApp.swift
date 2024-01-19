68c61538934f8f1fce4bba9983c3713984088a3a
Aloosha2 <alicjam@uw.edu>
2024-01-18 22:27:40 -0800

    added hello world
    added hello world

---
+++ hello-world/hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Alicja Misiuda on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
